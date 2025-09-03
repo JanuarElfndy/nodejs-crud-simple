38ba42c465a9fff488da0fca89ac82c3f1366bde
alydfazz <alya.azzahra.difa@gmail.com>
2025-09-03 14:02:33 +0700

    2nd commit

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
-// server.js
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const userRoutes = require('./routes/userRoutes');
-const path = require('path'); // Tambahkan ini
 
 dotenv.config();
-
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json()); // Untuk parsing body JSON
-
-// Tambahkan ini untuk melayani file statis dari folder 'public'
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rute API
 app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
-  // Sekarang, rute root akan melayani index.html dari folder public
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server berjalan di http://0.0.0.0:${port}`);
 });
